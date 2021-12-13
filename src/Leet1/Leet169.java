ea17126d1078fd8c7659f071f873aa0769df9523
whz <3147728559@qq.com>
2021-12-14 00:35:18 +0800

    增加Leet169的一种新的解法

--- src/Leet1/Leet169.java
+++ src/Leet1/Leet169.java
@@ -16,6 +16,7 @@ public class Leet169 {
         System.out.println(maxTimesNumber( input ));
         System.out.println(maxTimesNumber2( input ));
         System.out.println(maxTimesNumber3( input,0,input.length - 1));
+        System.out.println( maxTimesNumber4( input ) );
     }
 
     //常规方法,统计出现次数最多的元素，判断 出现个数是否大于n/2(向下取整) ，用到了Map,但不是最佳解法
@@ -39,7 +40,14 @@ public class Leet169 {
         return maxNum;
     }
 
-    //摩尔投票法,最佳解，正确性由  异国士兵极限一换一 可证明
+    /**
+     * 摩尔投票法,最佳解，正确性由 异国士兵极限一换一的例子 来证明
+     * 异国士兵极限一换一：
+     *    数组中不重复的数字总共有m个，可以假设有m个国家
+     *    每个重复的数字个数设置为k，那么当遇到两个数字不同时，就可以都消去，最后留下的肯定是重复超过二分之一的数字
+     * @param nums 输入数组
+     * @return
+     */
     public static int maxTimesNumber2( int[] nums ) {
 
         int times = 1;
@@ -61,7 +69,13 @@ public class Leet169 {
         return num;
     }
 
-    //分治法 nlogn 的事件复杂度,发现该方法只在 存在 个数大于n/2(向下取整) 的元素 时有效
+    /**
+     * 分治法 nlogn 的事件复杂度,发现该方法只在 存在 个数大于n/2(向下取整) 的元素 时有效。
+     * @param nums
+     * @param start
+     * @param end
+     * @return 返回值为数组中重复数字最多的数字
+     */
     public static int maxTimesNumber3( int[] nums ,int start,int end) {
 
         if( start == end ) {
@@ -89,6 +103,61 @@ public class Leet169 {
     // 还可以使用 排序的方法！！然后取 num[num.length/2]
 
     //还可以使用 计算32位bit的方法，事件复杂度位O(n) 如果 存在 k 个数 > n.length/2 那么它的每一位二进制个数也必然 > n.length/2
-    // 这是基数排序的思想
+    //当过没有过半的时，说明过半的数字为0
 
+    /**
+     * 另外一种方法，由剑指offer得到 : 通过时间复杂度为O(N)的求中间位置的快速排序中的parttion算法变形
+     * @param values
+     * @return
+     */
+    public static int maxTimesNumber4( int[] values ) {
+        int start = 0;
+        int end = values.length - 1;
+        while ( true ) {
+            int pivot = partition(values, start, end);
+            if( pivot == values.length/2 ) {
+                return values[pivot];
+            }else if( pivot < values.length/2 ) {
+                start = pivot + 1;
+            }else {
+                end = pivot - 1;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param values
+     * @param start
+     * @param end
+     * @return 返回 partition后的中枢下标位置，当start > end 时，返回 - 1
+     */
+    private static int partition( int[] values,int start,int end ) {
+
+        if( start > end ) { return -1; }
+        if( start == end ) { return start; }
+
+        int pivot = start + (int)(( end - start ) * Math.random());
+        //交换
+        int temp = values[pivot];
+        values[pivot] = values[start];
+        values[start] = temp;
+
+        int begin = start + 1,last = end;
+        while ( begin <= last ) {
+            while ( begin <= last && values[begin] <= values[start] ) { begin++; }
+            while ( begin <= last && values[last] > values[start] ) { last--; }
+            if( begin <= last ) {
+                temp = values[begin];
+                values[begin] = values[last];
+                values[last] = temp;
+            }
+        }
+        //此时start + 1 ~ begin - 1 的元素都满足 val <= values[start]
+        temp = values[start];
+        values[start] = values[begin - 1];
+        values[begin-1] = temp;
+
+        return begin - 1;
+    }
 }
